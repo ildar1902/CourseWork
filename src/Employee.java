11eee613b08ca8be12962721d757262ccf3ae576
Home <ildar86@live.ru>
2022-11-26 12:22:28 +0500

    добавил статическую переменную-счётчик и поле id. создал конструктор

--- src/Employee.java
+++ src/Employee.java
@@ -4,4 +4,15 @@ public class Employee {
     String patronymic;
     int department;
     double salary;
+    int id;
+    public static int idCounter; //статическая переменная счетчик, отвечающая за id
+
+    public Employee(String surname, String name, String patronymic, int department, double salary) {
+        this.surname = surname;
+        this.name = name;
+        this.patronymic = patronymic;
+        this.department = department;
+        this.salary = salary;
+        id = idCounter++;
+    }
 }
